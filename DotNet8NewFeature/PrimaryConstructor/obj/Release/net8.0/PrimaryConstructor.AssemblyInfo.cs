5a185d829dbb2c36e767511a4b4057e80205c1a6
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-05-07 15:13:12 +0300

    Initial commit

--- DotNet8NewFeature/PrimaryConstructor/obj/Release/net8.0/PrimaryConstructor.AssemblyInfo.cs
+++ DotNet8NewFeature/PrimaryConstructor/obj/Release/net8.0/PrimaryConstructor.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimaryConstructor")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4743f87af5fcb46cbf7e54f5f54f6edfe514cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimaryConstructor")]
