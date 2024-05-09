e33362b08a27b169715889f442a9e9263d51a848
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-05-09 11:40:00 +0300

    add new dot net 8.0 array feature

---
+++ DotNet8NewFeature/ArraySample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+int[] ar1 = [1, 2, 3];
+int[] ar2 = [4, 5, 6];
+int[] ar3 = [7, 8, 9];
+int[] bigArr1 = [.. ar1, .. ar2, .. ar3];
+
+Console.WriteLine("bigArr1");
+foreach (var item in bigArr1)
+{
+    Console.WriteLine(item.ToString());
+}
+Console.WriteLine("bigArr1");
+int[] bigArr2 = [.. ar1, 20, .. ar2, .. ar3];
+Console.WriteLine("bigArr2");
+foreach (var item in bigArr2)
+{
+    Console.WriteLine(item.ToString());
+}
+Console.WriteLine("bigArr2");
