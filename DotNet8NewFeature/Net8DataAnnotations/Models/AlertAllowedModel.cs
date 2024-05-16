970e2bba485bb505083fc0abbbc4d48e38c1a52c
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-05-16 15:23:57 +0300

    add AllowedValues model

--- DotNet8NewFeature/Net8DataAnnotations/Models/AlertAllowedModel.cs
+++ DotNet8NewFeature/Net8DataAnnotations/Models/AlertAllowedModel.cs
@@ -5,7 +5,7 @@ namespace Net8DataAnnotations.Models
     public class AlertAllowedModel
     {
 
-        [DeniedValues("Red", "Yellow", "Green")]
+        [AllowedValues("Red", "Yellow", "Green")]
         public string? AlertValue { get; set; }
 
     }
